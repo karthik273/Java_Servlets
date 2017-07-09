f7cef1f0c7f509cfb1e19692b0d07224ffab245b
Karthik <karthikthnr2@gmail.com>
2017-07-09 17:50:15 -0500

    Created a servlet

---
+++ src/karthik/servlets/SimpleServlet.java
@@ -0,0 +1,30 @@
+package karthik.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServletPath")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from me");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h3>Hello from me</h3>");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
